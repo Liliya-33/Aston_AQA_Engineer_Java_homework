ddf32e3fe5dc30a4da3691f67a1e51b0e6a8a34f
Liliya Kadyrova <liliya.kadyrova.0106@gmail.com>
2024-12-24 20:29:26 +0500

    ДЗ 14 урока, 2 задание

---
+++ src/main/java/org/example/lesson14_testng/FactorialCalculator.java
@@ -0,0 +1,13 @@
+package org.example.lesson14_testng;
+
+public class FactorialCalculator {
+    public static long calculateFactorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Число должно быть неотрицательным");
+        }
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * calculateFactorial(n - 1);
+    }
+}
